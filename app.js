dbc5d926dddf8ecd4250acc674419e3b3649babb
Michael <102651644+prodmichael@users.noreply.github.com>
2023-08-03 11:41:27 +0300

    add: util

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const cardsRoutes = require('./routes/credit-cards')
 const transactionsRoutes = require('./routes/transactions')
 const app = express()
+
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(require('cors')())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
